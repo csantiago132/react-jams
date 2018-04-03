cc78403af0ab59216376a2a0499d01f553d20a08
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-03 18:47:34 -0500

    LandingPage content updated

--- app/containers/LandingPage/LandingPage.js
+++ app/containers/LandingPage/LandingPage.js
@@ -12,6 +12,7 @@ const LandingPage = () => (
       <section className="landing">
         <h1 className="hero-title">Turn the music up!</h1>
       </section>
+
       <section className="selling-points">
         <article className="point">
           <h2 className="point-title">Choose your music</h2>
@@ -20,12 +21,14 @@ const LandingPage = () => (
             that someone else chose?
           </p>
         </article>
+
         <article className="point">
           <h2 className="point-title">Unlimited, streaming, ad-free</h2>
           <p className="point-description">
             No arbitrary limits. No distractions.
           </p>
         </article>
+
         <article className="point">
           <h2 className="point-title">Mobile enabled</h2>
           <p className="point-description">
