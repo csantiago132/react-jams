052dc86be9385966318e5211f25bae167f5ccb33
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-03 21:20:46 -0500

    adding dataFile

--- app/containers/LibraryPage/albumData.js
+++ app/containers/LibraryPage/albumData.js
@@ -1,77 +1,83 @@
-/* 
-* to avoid retyping the path, also, if path changes,
-* only one place to update
-*/
-const audioFilePath = '../../assets/music';
-const mainImageFilePath = '../../assets/images/album_covers';
+/* eslint-disable */
+
+// import images
+import image1 from '../../assets/images/album_covers/01.jpg';
+import image2 from '../../assets/images/album_covers/02.jpg';
+
+// import audio
+import audioBlue from '../../assets/music/blue.mp3';
+import audioGreen from '../../assets/music/green.mp3';
+import audioRed from '../../assets/music/red.mp3';
+import audioPink from '../../assets/music/pink.mp3';
+import audioMagenta from '../../assets/music/pink.mp3';
 
 export default [
   {
     title: 'The Colors',
     artist: 'Pablo Picasso',
     releaseInfo: '1909 Spanish Records',
-    albumCover: `${mainImageFilePath}/01.jpg`,
+    albumCover: image1,
     slug: 'the-colors',
     songs: [
       {
         title: 'Blue',
         duration: '161.71',
-        audioSrc: `${audioFilePath}/blue.mp3`,
+        audioSrc: audioBlue
       },
       {
         title: 'Green',
         duration: '103.96',
-        audioSrc: `${audioFilePath}/green.mp3`,
+        audioSrc: audioGreen
       },
       {
         title: 'Red',
         duration: '268.45',
-        audioSrc: `${audioFilePath}/red.mp3`,
+        audioSrc: audioRed
       },
       {
         title: 'Pink',
         duration: '153.14',
-        audioSrc: `${audioFilePath}/pink.mp3`,
+        audioSrc: audioPink
       },
       {
         title: 'Magenta',
         duration: '374.22',
-        audioSrc: `${audioFilePath}/magenta.mp3`,
-      },
-    ],
+        audioSrc: audioMagenta
+      }
+    ]
   },
   {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
     releaseInfo: '1909 EM',
-    albumCover: `${mainImageFilePath}/02.jpg`,
+    albumCover: image2,
     slug: 'the-telephone',
     songs: [
       {
         title: 'Blue',
         duration: '161.71',
-        audioSrc: `${audioFilePath}/blue.mp3`,
+        audioSrc: audioBlue
       },
       {
         title: 'Green',
         duration: '103.96',
-        audioSrc: `${audioFilePath}/green.mp3`,
+        audioSrc: audioGreen
       },
       {
         title: 'Red',
         duration: '268.45',
-        audioSrc: `${audioFilePath}/red.mp3`,
+        audioSrc: audioRed
       },
       {
         title: 'Pink',
         duration: '153.14',
-        audioSrc: `${audioFilePath}/pink.mp3`,
+        audioSrc: audioPink
       },
       {
         title: 'Magenta',
         duration: '374.22',
-        audioSrc: `${audioFilePath}/magenta.mp3`,
-      },
-    ],
-  },
+        audioSrc: audioMagenta
+      }
+    ]
+  }
 ];
