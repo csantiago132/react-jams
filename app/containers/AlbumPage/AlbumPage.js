b0e6a18c1bfac6403788d70e009b830a2add3fa2
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-04 19:00:33 -0500

    restructuring app

--- app/containers/AlbumPage/AlbumPage.js
+++ app/containers/AlbumPage/AlbumPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-
+// import albumData from '../../setup/data/data';
 /* 
 * since theres no state to pass, 
 * will leave as stateless function
