664e51c81d6986c5a20fd0b6a0880f69f660ef76
Carlos Fernandez <36578444+enrique0921@users.noreply.github.com>
2018-04-04 19:18:47 -0500

    adding details on album page

--- app/containers/AlbumPage/AlbumPage.js
+++ app/containers/AlbumPage/AlbumPage.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-// import albumData from '../../setup/data/data';
-/* 
-* since theres no state to pass, 
-* will leave as stateless function
-*/
-const AlbumPage = (props) => {
-  const { match } = props;
-  return (
-    <React.Fragment>
-      <Helmet>
-        <title>Album Page</title>
-        <meta name="description" content="Bloc-Jams library page" />
-      </Helmet>
+import albumData from '../../setup/data/data';
 
-      <main>
-        <section className="album">
-          {match.params.slug} Album page will go here
-        </section>
-      </main>
-    </React.Fragment>
-  );
-};
+class AlbumPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { match } = this.props;
+
+    const album = albumData.find((album) => album.slug === match.params.slug);
+
+    this.state = {
+      album: album,
+    };
+  }
+
+  render() {
+    const { albumCover, title, artist, releaseInfo } = this.state.album;
+
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>{title}</title>
+          <meta
+            name="description"
+            content={`${title} album page, album produced by ${artist}, released on ${releaseInfo}`}
+          />
+        </Helmet>
+
+        <main>
+          <section id="album-info">
+            <img id="album-cover-art" src={albumCover} alt={albumCover} />
+            <div className="album-details">
+              <h1 id="album-title">{title}</h1>
+              <h2 className="artist">{artist}</h2>
+              <div id="release-info">{releaseInfo}</div>
+            </div>
+          </section>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 AlbumPage.propTypes = {
   match: PropTypes.shape({
