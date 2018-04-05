5e91a1f407732a0d311a00db3e6405cd4f2df01c
Carlos Fernandez <36578444+enrique0921@users.noreply.github.com>
2018-04-05 18:10:23 -0500

    finished assignment, passing number, title and duration to songs

--- app/components/SongListTable/SongListTable.js
+++ app/components/SongListTable/SongListTable.js
@@ -2,21 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SongListTable = (props) => {
-  const { number, title, duration, audio } = props;
+  const { number, title, duration } = props;
+
+  // using Date native library
+  // we need a date before converting,
+  // pasisng as null
+  const getTime = new Date(null);
+  // passing props.duration
+  // as value to parse
+  getTime.setSeconds(duration);
+  // converting time toString,
+  // using RegEx to remove extra zeroes
+  const convertTime = getTime
+    .toISOString()
+    .substr(11, 8)
+    .replace(/^[0:]+/, '');
 
   return (
     <tr className="album-song">
       <td>{number}</td>
       <td>{title}</td>
-      <td>{duration}</td>
-      <td>
-        <audio controls>
-          <source src={audio} type="audio/mpeg" />
-
-          {/* adding this for accesibility flag ESLint */}
-          <track kind="captions" {...props} />
-        </audio>
-      </td>
+      <td>{convertTime}</td>
     </tr>
   );
 };
@@ -25,7 +31,6 @@ SongListTable.propTypes = {
   number: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   duration: PropTypes.string.isRequired,
-  audio: PropTypes.string.isRequired,
 };
 
 export default SongListTable;
