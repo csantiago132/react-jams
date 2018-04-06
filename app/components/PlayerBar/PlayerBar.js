07101cd1d5659544f6a887fb847c6b4e8adba44d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-05 20:44:31 -0500

    adding PlayerBar component

---
+++ app/components/PlayerBar/PlayerBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+/* eslint-disable react/prefer-stateless-function */
+class PlayerBar extends React.Component {
+  // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return <section className="player-bar">player bar goes here</section>;
+  }
+}
+
+PlayerBar.propTypes = {};
+
+export default PlayerBar;
