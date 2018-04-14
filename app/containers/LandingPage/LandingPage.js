ef7c0a1ea71dee133e34896642013c37b442999d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-14 15:19:04 -0500

    started styling for homepage

--- app/containers/LandingPage/LandingPage.js
+++ app/containers/LandingPage/LandingPage.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import './LandingPage.scss';
 
 const LandingPage = () => (
   <React.Fragment>
     <Helmet>
-      <title>Welcome</title>
+      <title>Welcome to Bloc-Jams</title>
       <meta name="description" content="Bloc-Jams application homepage" />
     </Helmet>
 
-    <main>
-      <section className="landing">
-        <h1 className="hero-title">Turn the music up!</h1>
-      </section>
+    <main className="landing-page">
+      <section className="landing-page__container">
+        <h1 className="landing-page__hero-title">Turn the music up!</h1>
 
-      <section className="selling-points">
-        <article className="point">
-          <h2 className="point-title">Choose your music</h2>
-          <p className="point-description">
-            The world is full of music; why should you have to listen to music
-            that someone else chose?
-          </p>
-        </article>
+        <section className="landing-page__upsell">
+          <article className="landing-page__upsell--music">
+            <h2 className="point-title">Choose your music</h2>
+            <p className="point-description">
+              The world is full of music; why should you have to listen to music
+              that someone else chose?
+            </p>
+          </article>
 
-        <article className="point">
-          <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-          <p className="point-description">
-            No arbitrary limits. No distractions.
-          </p>
-        </article>
+          <article className="landing-page__upsell--unlimited">
+            <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+            <p className="point-description">
+              No arbitrary limits. No distractions.
+            </p>
+          </article>
 
-        <article className="point">
-          <h2 className="point-title">Mobile enabled</h2>
-          <p className="point-description">
-            Listen to your music on the go. This streaming service is available
-            on all mobile platforms.
-          </p>
-        </article>
+          <article className="landing-page__upsell--mobile">
+            <h2 className="point-title">Mobile enabled</h2>
+            <p className="point-description">
+              Listen to your music on the go. This streaming service is
+              available on all mobile platforms.
+            </p>
+          </article>
+        </section>
       </section>
     </main>
   </React.Fragment>
