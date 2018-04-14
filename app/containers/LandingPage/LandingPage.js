57c0cd91092f1d4374ff12ab0454779f91ce98f7
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-04-14 16:11:17 -0500

    adding final tweaks to homepage

--- app/containers/LandingPage/LandingPage.js
+++ app/containers/LandingPage/LandingPage.js
@@ -14,24 +14,27 @@ const LandingPage = () => (
         <h1 className="landing-page__hero-title">Turn the music up!</h1>
 
         <section className="landing-page__upsell">
-          <article className="landing-page__upsell--music">
-            <h2 className="point-title">Choose your music</h2>
-            <p className="point-description">
+          <article className="landing-page__upsell--music-tile">
+            <span className="landing-page__upsell__number">001</span>
+            <h2 className="landing-page__upsell__title">Choose your music</h2>
+            <p className="landing-page__upsell__description">
               The world is full of music; why should you have to listen to music
               that someone else chose?
             </p>
           </article>
 
-          <article className="landing-page__upsell--unlimited">
-            <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-            <p className="point-description">
+          <article className="landing-page__upsell--unlimited-tile">
+            <span className="landing-page__upsell__number">002</span>
+            <h2 className="landing-page__upsell__title">Unlimited streaming</h2>
+            <p className="landing-page__upsell__description">
               No arbitrary limits. No distractions.
             </p>
           </article>
 
-          <article className="landing-page__upsell--mobile">
-            <h2 className="point-title">Mobile enabled</h2>
-            <p className="point-description">
+          <article className="landing-page__upsell--mobile-tile">
+            <span className="landing-page__upsell__number">003</span>
+            <h2 className="landing-page__upsell__title">Mobile enabled</h2>
+            <p className="landing-page__upsell__description">
               Listen to your music on the go. This streaming service is
               available on all mobile platforms.
             </p>
